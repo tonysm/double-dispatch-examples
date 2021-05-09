68fa424d80cde9d3d2b452773463d84c16967270
Tony Messias <tonysm@hey.com>
2021-05-09 16:06:48 -0300

    Floats can add other Floats

--- tests/AddsNumbersTest.php
+++ tests/AddsNumbersTest.php
@@ -12,9 +12,28 @@ class Integer
     }
 }
 
+class FloatNumber
+{
+    public function __construct(public float $value)
+    {
+    }
+
+    public function add(FloatNumber $number): FloatNumber
+    {
+        return new FloatNumber($this->value + $number->value);
+    }
+}
+
 test('Integers can add other Integers', function () {
     $first = new Integer(10);
     $second = new Integer(5);
 
     $this->assertSame(15, $first->add($second)->value);
+});
+
+test('Floats can add other Floats', function () {
+    $first = new FloatNumber(10.0);
+    $second = new FloatNumber(5.0);
+
+    $this->assertSame(15.0, $first->add($second)->value);
 });
